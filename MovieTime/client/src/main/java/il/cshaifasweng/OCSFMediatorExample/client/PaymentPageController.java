0ab165d7b235914e9f6da0e54501937b97b6c572
Shir Avneri <shiravneri@gmail.com>
2021-05-18 20:51:38 +0300

    Added warning labels to PaymentPage
    A suitable warning label is shown if a field is not filled

--- MovieTime/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PaymentPageController.java
+++ MovieTime/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PaymentPageController.java
@@ -1,5 +1,7 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
+import java.time.LocalDate;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,30 +11,53 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class PaymentPageController {
-
     @FXML
     private TextField firstNameTextField;
+
+    @FXML
+    private Label firstNameWarningLabel;
 
     @FXML
     private TextField lastNameTextField;
 
     @FXML
+    private Label lastNameWarningLabel;
+
+    @FXML
     private TextField emailTextField;
+
+    @FXML
+    private Label emailWarningLabel;
 
     @FXML
     private TextField addressTextField;
 
     @FXML
+    private Label addressWarningLabel;
+
+    @FXML
     private TextField cityTextField;
+
+    @FXML
+    private Label cityWarningLabel;
 
     @FXML
     private TextField phoneNumberTextField;
 
     @FXML
+    private Label phoneNumberWarningLabel;
+
+    @FXML
     private TextField cardHoldersNameTextField;
 
     @FXML
+    private Label cardHoldersNameWarningLabel;
+
+    @FXML
     private TextField cardHoldersIDTextField;
+
+    @FXML
+    private Label cardHoldersIDWarningLabel;
 
     @FXML
     private ComboBox<?> paymentNumberComboBox;
@@ -41,10 +66,19 @@ public class PaymentPageController {
     private TextField cardNumberTextField;
 
     @FXML
+    private Label cardNumberWarningLabel;
+
+    @FXML
     private DatePicker cardExpirationDatePicker;
 
     @FXML
+    private Label cardExpirationDateWarningLabel;
+
+    @FXML
     private TextField cardCVVTextField;
+
+    @FXML
+    private Label cardCVVWarningLabel;
 
     @FXML
     private Label paymentLabel;
@@ -53,25 +87,120 @@ public class PaymentPageController {
     private Button payNowBtn;
 
     @FXML
-    void padNow(ActionEvent event) {
-
-    }
-
-    @FXML
     void initialize() {
         assert firstNameTextField != null : "fx:id=\"firstNameTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert firstNameWarningLabel != null : "fx:id=\"firstNameWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert lastNameTextField != null : "fx:id=\"lastNameTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert lastNameWarningLabel != null : "fx:id=\"lastNameWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert emailTextField != null : "fx:id=\"emailTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert emailWarningLabel != null : "fx:id=\"emailWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert addressTextField != null : "fx:id=\"addressTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert addressWarningLabel != null : "fx:id=\"addressWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert cityTextField != null : "fx:id=\"cityTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
-        assert phoneNumberTextField != null : "fx:id=\"zipCodeTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert cityWarningLabel != null : "fx:id=\"cityWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert phoneNumberTextField != null : "fx:id=\"phoneNumberTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert phoneNumberWarningLabel != null : "fx:id=\"phoneNumberWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert cardHoldersNameTextField != null : "fx:id=\"cardHoldersNameTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert cardHoldersNameWarningLabel != null : "fx:id=\"cardHoldersNameWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert cardHoldersIDTextField != null : "fx:id=\"cardHoldersIDTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert cardHoldersIDWarningLabel != null : "fx:id=\"cardHoldersIDWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert paymentNumberComboBox != null : "fx:id=\"paymentNumberComboBox\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert cardNumberTextField != null : "fx:id=\"cardNumberTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert cardNumberWarningLabel != null : "fx:id=\"cardNumberWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert cardExpirationDatePicker != null : "fx:id=\"cardExpirationDatePicker\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert cardExpirationDateWarningLabel != null : "fx:id=\"cardExpirationDateWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert cardCVVTextField != null : "fx:id=\"cardCVVTextField\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        assert cardCVVWarningLabel != null : "fx:id=\"cardCVVWarningLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert paymentLabel != null : "fx:id=\"paymentLabel\" was not injected: check your FXML file 'PaymentPage.fxml'.";
         assert payNowBtn != null : "fx:id=\"payNowBtn\" was not injected: check your FXML file 'PaymentPage.fxml'.";
+        
+        hideWarningLabels();
+    }
+    
+	void hideWarningLabels() {
+		firstNameWarningLabel.setVisible(false);
+		lastNameWarningLabel.setVisible(false);
+		emailWarningLabel.setVisible(false);
+		addressWarningLabel.setVisible(false);
+		cityWarningLabel.setVisible(false);
+		phoneNumberWarningLabel.setVisible(false);
+		cardHoldersNameWarningLabel.setVisible(false);
+		cardHoldersIDWarningLabel.setVisible(false);
+		cardNumberWarningLabel.setVisible(false);
+		cardExpirationDateWarningLabel.setVisible(false);
+		cardCVVWarningLabel.setVisible(false);
+	}
+
+    @FXML
+    void padNow(ActionEvent event) {
+    	hideWarningLabels();
+    	
+    	String firstName = firstNameTextField.getText();
+    	if(firstName == "") {
+    		firstNameWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	String lastName = lastNameTextField.getText();
+    	if(lastName == "") {
+    		lastNameWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	String email = emailTextField.getText();
+    	if(email == "") {
+    		emailWarningLabel.setVisible(true);
+    		return;
+    	}
+    		
+    	String address = addressTextField.getText();
+    	if(address == "") {
+    		addressWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	String city = cityTextField.getText();
+    	if(city == "") {
+    		cityWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	String phoneNumber = phoneNumberTextField.getText();
+    	if(phoneNumber == "") {
+    		phoneNumberWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	String cardHoldersName = cardHoldersNameTextField.getText();
+    	if(cardHoldersName == "") {
+    		cardHoldersNameWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	String cardHoldersID = cardHoldersIDTextField.getText();
+    	if(cardHoldersID == "") {
+    		cardHoldersIDWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	String cardNumber = cardNumberTextField.getText();
+    	if(cardNumber == "") {
+    		cardNumberWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	LocalDate cardExpirationDate = cardExpirationDatePicker.getValue();
+    	if(cardExpirationDate == null) {
+    		cardExpirationDateWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	String cardCVV = cardCVVTextField.getText();
+    	if(cardCVV == "") {
+    		cardCVVWarningLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	// Else - save order in database
     }
 }

8590972a941e0ce77be72ef76013702c3ea5c43c
vegeta221 <64312436+vegeta221@users.noreply.github.com>
2021-05-23 21:19:20 +0300

    new pages still needs work

---
+++ MovieTime/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/ocsf/PurchaseCancelationPageController.java
@@ -0,0 +1,64 @@
+package il.cshaifasweng.OCSFMediatorExample.client.ocsf;
+
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PurchaseCancelationPageController {
+
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private TextField OrderTextField;
+
+	@FXML
+	private Label OrderWarningLabel;
+
+	@FXML
+	private Button SearchNowBtn(ActionEvent event)
+	{
+		
+		return null;		
+	}
+
+	@FXML
+	void padNow(ActionEvent event) {
+    	hideWarningLabels();
+    	boolean emptyField = true;
+    	
+    	String Order = OrderTextField.getText();
+    	if(Order == "")
+    	{
+    		OrderWarningLabel.setVisible(true);
+    		emptyField = false;
+    	}
+    	
+    	if(emptyField == false) {
+    		return;
+    	}
+    	
+    	
+	}
+
+	@FXML
+	void initialize() {
+		assert OrderTextField != null
+				: "fx:id=\"OrderTextField\" was not injected: check your FXML file 'PurchaseCancelation.fxml'.";
+		assert OrderWarningLabel != null
+				: "fx:id=\"OrderWarningLabel\" was not injected: check your FXML file 'PurchaseCancelation.fxml'.";
+		assert SearchNowBtn != null
+				: "fx:id=\"SearchNowBtn\" was not injected: check your FXML file 'PurchaseCancelation.fxml'.";
+		
+		hideWarningLabels();
+
+	}
+
+	private void hideWarningLabels() {
+		OrderWarningLabel.setVisible(false);
+	}
+
+}

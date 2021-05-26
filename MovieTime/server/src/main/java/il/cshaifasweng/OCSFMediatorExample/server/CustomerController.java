69e3767d561e835e4f60388fe09eae2626a49a78
Shir Avneri <shiravneri@gmail.com>
2021-05-26 23:43:18 +0300

    Fixed a bug

--- MovieTime/server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/CustomerController.java
+++ MovieTime/server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/CustomerController.java
@@ -2,63 +2,14 @@ package il.cshaifasweng.OCSFMediatorExample.server;
 
 
 import java.util.ArrayList;
-import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
 import il.cshaifasweng.OCSFMediatorExample.entities.Complaint;
 import il.cshaifasweng.OCSFMediatorExample.entities.Purchase;
 import javafx.util.Pair;
 
 
-public class CustomerController{
-	static SessionFactory sessionFactory = Main.getSessionFactory();
-	private static Session session;
-	
-//	public static <T> void saveRowInDB(T objectType) {
-//		try {
-//			session = sessionFactory.openSession();
-//			session.beginTransaction();
-//			session.save(objectType);
-//			session.flush();
-//			session.getTransaction().commit();
-//			session.clear();
-//		} catch (Exception e) {
-//			if (session != null) {
-//				session.getTransaction().rollback();
-//			}
-//			System.err.println("An error occured, changes have been rolled back.");
-//			e.printStackTrace();
-//		} finally
-//		{
-//			assert session != null;
-//			session.close();
-//			System.out.println("saveCustomerInDB");
-//		}
-//	}
-//	public static <T> void updateDB(T objectType) {
-//		try {
-//			session = sessionFactory.openSession();
-//			session.beginTransaction();
-//			session.update(objectType);
-//			session.flush();
-//			session.getTransaction().commit();
-//			session.clear();
-//		} catch (Exception e) {
-//			if (session != null) {
-//				session.getTransaction().rollback();
-//			}
-//			System.err.println("An error occured, changes have been rolled back.");
-//			e.printStackTrace();
-//		} finally
-//		{
-//			assert session != null;
-//			session.close();
-//			System.out.println("Complaint added to database");
-//		}
-//	}
-	
+public class CustomerController{	
+
 	public static ArrayList<Complaint> getAllCurrentComplaints() {
 		ArrayList<Complaint> screeningComplaintArrayList = new ArrayList<>();
 		ArrayList<Complaint> toReturnArrayList = new ArrayList<>();

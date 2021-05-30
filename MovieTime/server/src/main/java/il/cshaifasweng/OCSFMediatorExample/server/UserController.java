1b03c6e03d08b6659fd19e2fbd65f0a883b8dc53
nivsap <nivsap@gmail.com>
2021-05-30 17:24:31 +0300

    A user can only log in once at a time

--- MovieTime/server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/UserController.java
+++ MovieTime/server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/UserController.java
@@ -13,6 +13,7 @@ public class UserController {
 	
 	public static void getUser (Message msg) {
 		ArrayList<Worker> allWorkers = Main.getAllOfType(Worker.class);
+		int flag = 0;
 		for(Worker arr : allWorkers) {
 			if(arr.getUserName().equals(msg.getUsername()) && arr.getPassword().equals( msg.getPassword()) && arr.isLoggedIn()==false) {
 				if(arr instanceof BranchManager) {
@@ -20,31 +21,43 @@ public class UserController {
 					System.out.println(msg.getTypeOfWorkerString());
 					arr.setLoggedIn(true);
 					msg.setWorker(arr);
+					Main.updateRowDB(arr);
+					flag = 1;
 				}
 				else if(arr instanceof ContentManager) {
 					msg.setTypeOfWorkerString("ContentManager");
 					arr.setLoggedIn(true);
 					System.out.println(msg.getTypeOfWorkerString());
 					msg.setWorker(arr);
+					Main.updateRowDB(arr);
+					flag = 1;
 				}
 				else if(arr instanceof CustomerService) {
 					msg.setTypeOfWorkerString("CustomerService");
 					System.out.println(msg.getTypeOfWorkerString());
 					arr.setLoggedIn(true);
 					msg.setWorker(arr);
+					Main.updateRowDB(arr);
+					flag = 1;
 				}
 				else if(arr instanceof NetworkAdministrator) {
 					msg.setTypeOfWorkerString("NetworkAdministrator");
 					System.out.println(msg.getTypeOfWorkerString());
 					arr.setLoggedIn(true);
 					msg.setWorker(arr);
+					Main.updateRowDB(arr);
+					flag = 1;
 				}
 				else msg.setTypeOfWorkerString("null");
 			}
-			else if(arr.isLoggedIn()==true) {
+			else if(arr.getUserName().equals(msg.getUsername()) && arr.getPassword().equals( msg.getPassword()) &&arr.isLoggedIn()==true) {
 				msg.setTypeOfWorkerString("you are already logged in");
+				flag = 1;
 			}
 
+		}
+		if(flag==0) {
+			msg.setTypeOfWorkerString("This user does not exist");
 		}
 	}
 }

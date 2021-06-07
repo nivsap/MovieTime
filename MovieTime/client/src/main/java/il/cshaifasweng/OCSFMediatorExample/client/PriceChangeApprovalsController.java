885d1017e621dd52ffee7770a19a75cff892ede6
vegeta221 <64312436+vegeta221@users.noreply.github.com>
2021-06-07 22:09:43 +0300

    progress

--- MovieTime/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PriceChangeApprovalsController.java
+++ MovieTime/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PriceChangeApprovalsController.java
@@ -1,43 +1,18 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
-import java.io.IOException;
 import java.util.ResourceBundle;
 
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-
-import il.cshaifasweng.OCSFMediatorExample.entities.Message;
-import il.cshaifasweng.OCSFMediatorExample.entities.Purchase;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBase;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 public class PriceChangeApprovalsController {
-	private Boolean isRegistered;
-	@SuppressWarnings("unused")
-	private int waitingForMessageCounter;
-	
-	public PriceChangeApprovalsController() {
-		isRegistered = false;
-		waitingForMessageCounter = 0;
-	}
 
 	@FXML
 	private ResourceBundle resources;
-
-	@FXML
-	private Label PendintAlert;
-
-	@FXML
-	private Button PendingBtn;
 
 	@FXML
 	private Label DateTitle;
@@ -61,22 +36,10 @@ public class PriceChangeApprovalsController {
 	private Label CommentTitle;
 
 	@FXML
-	private TextField DateOfRequest;
-
-	@FXML
-	private TextField RequestType;
-
-	@FXML
-	private TextField ReasonField;
-
-	@FXML
-	private ComboBox<String> decisionBox;
+	private ComboBox<?> decisionBox;
 
 	@FXML
 	private Button PendingBtn1;
-
-	@FXML
-	private Label SubmitAlert;
 
 	@FXML
 	private Label ShowTheOldPrice;
@@ -88,50 +51,32 @@ public class PriceChangeApprovalsController {
 	private Label DecisionTitle;
 
 	@FXML
-	void PendReq(ActionEvent event) {
-		Message msg = new Message();
-		msg.setAction("got pending price change request");
-		sendMessageToServer(msg);
-	}
+	private Label numRequestLabel;
 
-	ObservableList<String> list = FXCollections.observableArrayList("Accept", "Reject");
+	@FXML
+	private Label ReasonShow;
 
-	private void sendMessageToServer(Message msg) {
-		try {
-			if (!isRegistered) {
-				EventBus.getDefault().register(this);
-				isRegistered = true;
-			}
-			AppClient.getClient().sendToServer(msg);
-			waitingForMessageCounter++;
-		} catch (IOException e) {
-			System.out.println("failed to send msg to server from PurchaseCancellationPage");
-			waitingForMessageCounter--;
-			e.printStackTrace();
-		}
-	}
+	@FXML
+	private Label RequestTypeShow;
+
+	@FXML
+	private Label DateShow;
+
+	@FXML
+	private Label numRequestShow;
 
 	@FXML
 	void ShowApproveDenied(ActionEvent event) {
-		
+
 	}
 
 	@FXML
 	void SubBtn(ActionEvent event) {
-		Message msg = new Message();
-		msg.setAction("price has changed");
-		sendMessageToServer(msg);
 
 	}
 
 	@FXML
 	void initialize() {
-		
-		EventBus.getDefault().register(this);
-		assert PendintAlert != null
-				: "fx:id=\"PendintAlert\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
-		assert PendingBtn != null
-				: "fx:id=\"PendingBtn\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
 		assert DateTitle != null
 				: "fx:id=\"DateTitle\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
 		assert RequestTitle != null
@@ -146,67 +91,28 @@ public class PriceChangeApprovalsController {
 				: "fx:id=\"CommentBox\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
 		assert CommentTitle != null
 				: "fx:id=\"CommentTitle\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
-		assert DateOfRequest != null
-				: "fx:id=\"DateOfRequest\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
-		assert RequestType != null
-				: "fx:id=\"RequestType\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
-		assert ReasonField != null
-				: "fx:id=\"ReasonField\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
 		assert decisionBox != null
 				: "fx:id=\"decisionBox\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
 		assert PendingBtn1 != null
 				: "fx:id=\"PendingBtn1\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
-		assert SubmitAlert != null
-				: "fx:id=\"SubmitAlert\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
 		assert ShowTheOldPrice != null
 				: "fx:id=\"ShowTheOldPrice\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
 		assert ShowTheNewPrice != null
 				: "fx:id=\"ShowTheNewPrice\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
 		assert DecisionTitle != null
 				: "fx:id=\"DecisionTitle\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
-		hideWarnings();
-		decisionBox.setItems(list);
+		assert numRequestLabel != null
+				: "fx:id=\"numRequestLabel\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
+		assert ReasonShow != null
+				: "fx:id=\"ReasonShow\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
+		assert RequestTypeShow != null
+				: "fx:id=\"RequestTypeShow\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
+		assert DateShow != null
+				: "fx:id=\"DateShow\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
+		assert numRequestShow != null
+				: "fx:id=\"numRequestShow\" was not injected: check your FXML file 'PriceChangeApprovals.fxml'.";
+		
+		Massage msg = new Message();
 
-	}
-
-	private void hideWarnings() {
-		PendintAlert.setVisible(false);
-		DateTitle.setVisible(false);
-		RequestTitle.setVisible(false);
-		OldPriceTitle.setVisible(false);
-		NewPriceTitle.setVisible(false);
-		ReasonTitle.setVisible(false);
-		CommentBox.setVisible(false);
-		CommentTitle.setVisible(false);
-		DateOfRequest.setVisible(false);
-		RequestType.setVisible(false);
-		ReasonField.setVisible(false);
-		decisionBox.setVisible(false);
-		PendingBtn1.setVisible(false);
-		SubmitAlert.setVisible(false);
-		ShowTheOldPrice.setVisible(false);
-		ShowTheNewPrice.setVisible(false);
-		DecisionTitle.setVisible(false);
-	}
-
-	@Subscribe
-	public void onMessageEvene(Message msg) {
-		if (msg.getAction().equals("got pending price change request")) {
-			Platform.runLater(() -> {
-				try {
-					App.setContent("OpenPriceChangeRequests");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			});
-
-			if (msg.getAction().equals("price has changed")) {
-				Platform.runLater(() -> {
-					hideWarnings();
-				});
-			}
-
-		}
 	}
 }

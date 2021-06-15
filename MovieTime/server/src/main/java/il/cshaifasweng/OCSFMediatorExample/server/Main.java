f608d25fbb68f6f15887365232a300185026fe4a
Shir Avneri <shiravneri@gmail.com>
2021-06-15 17:40:40 +0300

    Remove complaint type combobox

--- MovieTime/server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/Main.java
+++ MovieTime/server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/Main.java
@@ -247,9 +247,9 @@ public class Main extends AbstractServer {
 			Purchase customer11 = new Purchase("Asaf", "Moshe", "shiravneri@gmail.com", "street 11, city", "0523456789", 600.0, getTime(2021, 6, 5), null, card3, null);
 			card1.setPurchase(customer9); card2.setPurchase(customer10); card3.setPurchase(customer11); 
 			/* ---------- Setting Complaints For Data Base ---------- */
-			Complaint complaint1 = new Complaint("Shir", "Avneri", "shiravneri@gmail.com", "0523456789", Complaint.getComplaintTypes()[0], "I'm very upset", "I want to finish this project", customer1, true);
-			Complaint complaint2 = new Complaint("Niv", "Sapir", "shiravneri@gmail.com", "0523456789", Complaint.getComplaintTypes()[0], "I want to complain", "I am very upset", customer2, true);
-			Complaint complaint3 = new Complaint("Hadar", "Manor", "shiravneri@gmail.com", "0523456789", Complaint.getComplaintTypes()[0], "Some title", "Some details", customer3, false);
+			Complaint complaint1 = new Complaint("Shir", "Avneri", "shiravneri@gmail.com", "0523456789", "I'm very upset", "I want to finish this project", customer1, true);
+			Complaint complaint2 = new Complaint("Niv", "Sapir", "shiravneri@gmail.com", "0523456789", "I want to complain", "I am very upset", customer2, true);
+			Complaint complaint3 = new Complaint("Hadar", "Manor", "shiravneri@gmail.com", "0523456789", "Some title", "Some details", customer3, false);
 			
 			haifaCinema.setComplaints(new ArrayList<Complaint>(Arrays.asList(complaint1, complaint2))); 
 			telAvivCinema.setComplaints(new ArrayList<Complaint>(Arrays.asList(complaint3)));
